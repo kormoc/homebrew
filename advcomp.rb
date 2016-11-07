0bec726741df49ca269e3fbeafb19a49961b40aa
Rob Smith <kormoc@gmail.com>
2016-11-06 23:03:36 -0800

    advcomp version bump

--- advcomp.rb
+++ advcomp.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Advcomp < Formula
   homepage 'http://advancemame.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/advancemame/advancecomp/1.17/advancecomp-1.17.tar.gz'
-  sha256 '856d064e064c6105986b7cc29d6e2cba477e9ffca4a8830631161f5e7cd18e82'
+  url 'http://downloads.sourceforge.net/project/advancemame/advancecomp/1.19/advancecomp-1.19.tar.gz'
+  sha256 'd594c50c3da356aa961f75b00e958a4ed1e142c6530b42926092e46419af3047'
 
   def install
     system "./configure"
